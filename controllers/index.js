be825a3daf27b0c340e4bcf7d4a9287ddc9b85bd
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-28 13:16:53 -0400

    can view landing page

--- controllers/index.js
+++ controllers/index.js
@@ -2,26 +2,26 @@ const router = require('express').Router();
 const {Post} = require('../models')
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
+router.get('/', async (req, res) => {res.render('landing');});
+// router.get('/', async (req, res) => 
+    // const postData = await Post.findAll({
+    //     include: [    
+    //     {   
+    //         model: User,
+    //         attributes: ['username'], 
+    //     },
 
-router.get('/', async (req, res) => {
-    const postData = await Post.findAll({
-        include: [    
-        {   
-            model: User,
-            attributes: ['username'], 
-        },
-
-    ]
-    }).catch((err) => { 
+    // ]
+    // }).catch((err) => { 
         
-        res.json(err);
-    });
-    const posts = postData.map((post) => post.get({ plain: true }));
-    // res.status(200).json(posts)
-        res.render('homepage', { 
-            posts, 
-            loggedIn: req.session.loggedIn
-        });
-    });
+    //     res.json(err);
+    // });
+    // const posts = postData.map((post) => post.get({ plain: true }));
+    // // res.status(200).json(posts)
+    //     res.render('homepage', { 
+    //         posts, 
+    //         loggedIn: req.session.loggedIn
+    //     });
+    // });
 
     module.exports = router;
