1e82f0f5a0b97a74b6bf54719be499d06edb5a02
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-29 10:27:27 -0400

    all post route

--- controllers/index.js
+++ controllers/index.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
-const {Post} = require('../models')
+const {Post, User} = require('../models')
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
-router.get('/', async (req, res) => {res.render('landing');});
+// router.get('/', async (req, res) => {res.render('landing');});
 router.get('/signup', async (req, res) => {res.render('signup');});
 router.get('/login', async (req, res) => {res.render('login');});
-// router.get('/', async (req, res) => 
-    // const postData = await Post.findAll({
-    //     include: [    
-    //     {   
-    //         model: User,
-    //         attributes: ['username'], 
-    //     },
+router.get('/postings', async (req, res) => {
+    const postData = await Post.findAll({
+        include: [    
+        {   
+            model: User,
+            attributes: ['username'], 
+        },
 
-    // ]
-    // }).catch((err) => { 
+    ]
+    }).catch((err) => { 
         
-    //     res.json(err);
-    // });
-    // const posts = postData.map((post) => post.get({ plain: true }));
-    // // res.status(200).json(posts)
-    //     res.render('homepage', { 
-    //         posts, 
-    //         loggedIn: req.session.loggedIn
-    //     });
-    // });
+        res.json(err);
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // res.status(200).json(posts)
+        res.render('postings', { 
+            posts, 
+            loggedIn: req.session.loggedIn
+        });
+    });
 
     module.exports = router;
