af4dbe3c0a33a0366345cff72b7429bb6023c097
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-29 21:48:27 -0400

    styled posts home button links to posts

--- controllers/api/post.routes.js
+++ controllers/api/post.routes.js
@@ -1,24 +1,5 @@
-const router = require('express').Router();
-const {Post, User} = require('../../models/');
+// const router = require('express').Router();
+// const {Post, User} = require('../../models/');
 
-router.get('/postings', async (req, res) => {
-    const postData = await Post.findAll({
-        include: [    
-        {   
-            model: User,
-            attributes: ['username'], 
-        },
 
-    ]
-    }).catch((err) => { 
-        
-        res.json(err);
-    });
-    const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('postings', { 
-            posts, 
-            loggedIn: req.session.loggedIn
-        });
-    });
-
-    module.exports = router;
+//     module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {Post, User} = require('../models/');
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
@@ -6,5 +7,24 @@ router.get('/', async (req, res) => {res.render('landing',{loggedIn: req.session
 router.get('/signup', async (req, res) => {res.render('signup',{loggedIn: req.session.loggedIn});});
 router.get('/login', async (req, res) => {res.render('login',{loggedIn: req.session.loggedIn});});
 
+router.get('/postings', async (req, res) => {
+    const postData = await Post.findAll({
+        include: [    
+        {   
+            model: User,
+            attributes: ['username'], 
+        },
+
+    ]
+    }).catch((err) => { 
+        
+        res.json(err);
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('postings', { 
+            posts, 
+            loggedIn: req.session.loggedIn
+        });
+    });
 
 module.exports = router;
